c4c76801d689ea724e72ae3e82b44092adc7283c
Ian Yoel Gómez Oses <148839935+Iagomez05@users.noreply.github.com>
2024-02-29 20:58:05 -0600

    Add files via upload

---
+++ Community-Music-Player/README.md
@@ -0,0 +1,2 @@
+# Community-Music-Player
+Proyecto de Algoritmos y Estructuras de Datos 1, 1er Semestre 2023
---
+++ Community-Music-Player/cliente/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Establecer la dirección IP y el puerto del servidor
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                int puerto = 6400;
+
+                // Crear el socket TCP/IP
+                Socket clienteSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Conectar el socket al servidor
+                clienteSocket.Connect(ipAddress, puerto);
+
+                Console.WriteLine("Conexión establecida con el servidor.");
+
+                // Enviar datos al servidor
+                Console.Write("Ingrese el mensaje a enviar: ");
+                string mensaje = Console.ReadLine() + "\n"; // Agregar un salto de línea al final del mensaje
+                byte[] mensajeBytes = Encoding.UTF8.GetBytes(mensaje);
+                clienteSocket.Send(mensajeBytes);
+
+                // Recibir respuesta del servidor
+                byte[] buffer = new byte[1024];
+                int bytesRecibidos = clienteSocket.Receive(buffer);
+                string respuesta = Encoding.UTF8.GetString(buffer, 0, bytesRecibidos);
+                Console.WriteLine("Respuesta del servidor: " + respuesta);
+
+                // Cerrar el socket
+                clienteSocket.Shutdown(SocketShutdown.Both);
+                clienteSocket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.ToString());
+            }
+        }
+    }
+}
