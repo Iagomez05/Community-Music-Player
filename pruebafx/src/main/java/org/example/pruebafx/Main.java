9fff4b62738fd2d8a7a7441bc633a5750ba56674
Fabiola <fmelendezsequeira@gmail.com>
2024-03-17 21:43:28 -0600

    Reproductor con Linked Lists

---
+++ pruebafx/src/main/java/org/example/pruebafx/Main.java
@@ -0,0 +1,24 @@
+package org.example.pruebafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("music_player.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 969, 591);
+        stage.setTitle("Music Player");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
